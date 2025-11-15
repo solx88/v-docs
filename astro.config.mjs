0258aef71f061119b24b2936abaaa8b7cc4a9c1b
Tuan Le <tuanlva88@outlook.com>
2025-11-15 16:18:32 +0700

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,42 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import starlightThemeRapide from "starlight-theme-rapide";
+import starlightImageZoom from "starlight-image-zoom";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: "My Docs",
+      // social: [
+      //   {
+      //     icon: "github",
+      //     label: "GitHub",
+      //     href: "https://github.com/withastro/starlight",
+      //   },
+      // ],
+
+      sidebar: [
+        {
+          label: "Phiếu in, báo cáo",
+          items: [],
+        },
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", slug: "guides/example" },
+            { label: "Example Guide1", slug: "guides/ex1" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+      plugins: [starlightThemeRapide(), starlightImageZoom()],
+      customCss: ["./src/styles/custom.css"],
+    }),
+  ],
 });
